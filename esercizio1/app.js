7be87b06fa94f541db7cbfe97c04d8a120bc5977
Andrea <andrea.raise95@gmail.com>
2020-04-09 20:25:10 +0200

    added esercizio 1

---
+++ esercizio1/app.js
@@ -0,0 +1,74 @@
+var app = new function(){
+    this.userView = document.getElementById("userList");
+
+    this.users = [
+        new User('Andrea','Raise'),
+        new User('Matteo','Trentin')
+    ];
+
+    this.showList = function(){
+        data = ''
+        this.users.forEach(function(user,i) {
+        data += '<div class = "userDiv">';
+        data +=  user.name + ' ' + user.lastname
+        data += '<button onclick="app.Edit(' + i + ')">Edit</button>';
+        data += '<button onclick="app.Delete(' + i + ')">Delete</button>';
+        data += '<div>';
+        });
+        return this.userView.innerHTML = data;
+    };
+
+    this.Add = function () {
+        elName = document.getElementById('add-name');
+        elLastname = document.getElementById('add-lastname')
+        // Get the value
+        var userToAdd = new User(elName.value,elLastname.value)
+        if (userToAdd) {
+          // Add the new value
+          this.users.push(userToAdd);
+          // Reset input value
+          elName.value = '';
+          elLastname.value = '';
+          // Dislay the new list
+          this.showList();
+        }
+    };
+
+    this.Edit = function (item) {
+        var elName = document.getElementById('edit-name');
+        var elLastname = document.getElementById('edit-lastname')
+        // Display value in the field
+        var userToUpdate = this.users[item];
+        elName.value = userToUpdate.name;
+        elLastname.value = userToUpdate.lastname;
+        // Display fields
+        document.getElementById('spoiler').style.display = 'block';
+        self = this;
+    
+        document.getElementById('saveEdit').onsubmit = function() {
+          // Get value
+          var userUpdated = new User(elName.value,elLastname.value);
+    
+          if (userUpdated) {
+            // Edit value
+            self.users.splice(item, 1, userUpdated);
+            // Display the new list
+            self.showList();
+            // Hide fields
+            CloseInput();
+          }
+        }
+    };
+    
+      this.Delete = function (item) {
+        // Delete the current row
+        this.users.splice(item, 1);
+        // Display the new list
+        this.showList();
+      };
+}
+app.showList();
+
+function CloseInput() {
+    document.getElementById('spoiler').style.display = 'none';
+}
